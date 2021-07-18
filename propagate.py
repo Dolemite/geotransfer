5efba230ba9b494bbd17a972b10aafa3182d09c2
Brent Smith <brent@brentsmith.io>
2021-07-17 21:42:06 -0400

    Code cleanup and output reformatting

--- propagate.py
+++ propagate.py
@@ -5,6 +5,7 @@ from sgp4 import exporter
 from sgp4.conveniences import jday_datetime
 import numpy as np
 import socket
+import json
 
 # Standard gravitational parameter for Earth
 u = 3.9860044188E14
@@ -71,6 +72,8 @@ class OrbitalElements:
         mm = 86400 / T
         mm_r = mm * (1 / 1440) * (2 * math.pi)  # convert to radians/minute
 
+        print("           Orbital Elements Summary           ")
+        print("==============================================")
         print("Semimajor axis, a (km): ", semimajor_axis / 1000)
         print("Eccentricity, e: ", eccentricity)
         print("Inclination, i (deg): ", inclination * (180.0 / math.pi))
@@ -79,8 +82,10 @@ class OrbitalElements:
         print("True anomaly, υ (deg): ", nu * (180.0 / math.pi))
         print("Mean anomaly, M (deg): ", M * (180.0 / math.pi))
         print("Orbital period, T (sec): ", T)
-        print("Mean motion (revs/day): ", mm)
+        print("Mean motion, n (revs/day): ", mm)
         print("Mean motion (radians/min): ", mm_r)
+        print("==============================================")
+        print()
 
         self.semimajor_axis = semimajor_axis
         self.eccentricity = eccentricity
@@ -93,13 +98,13 @@ class OrbitalElements:
         self.mm = mm_r
 
 
-def get_hohmann_dv1(apogee_distance, perigee_distance):
+def get_hohmann_transfer_dv1(apogee_distance, perigee_distance):
     deltav = math.sqrt(u / (perigee_distance * 1000)) * (
                 math.sqrt((2 * apogee_distance * 1000) / ((apogee_distance * 1000) + (perigee_distance * 1000))) - 1)
     return deltav
 
 
-def get_hohmann_dv2(apogee_distance, perigee_distance):
+def get_hohmann_transfer_dv2(apogee_distance, perigee_distance):
     deltav = math.sqrt(u / (apogee_distance * 1000)) * (
                 1 - math.sqrt((2 * perigee_distance * 1000) / ((apogee_distance * 1000) + (perigee_distance * 1000))))
     return deltav
@@ -107,6 +112,19 @@ def get_hohmann_dv2(apogee_distance, perigee_distance):
 
 def vis_viva(radius, semi_major_axis):
     return math.sqrt(u * ((2 / (radius * 1000)) - (1 / (semi_major_axis * 1000))))
+
+
+def read_input(filename):
+    data = {}
+    with open(filename, 'r') as fp:
+        data = json.load(fp)
+    return data
+
+
+def save_tle(filename, line1, line2):
+    with open(filename, 'w') as fp:
+        print(line1, file=fp)
+        print(line2, file=fp)
 
 
 def submit_answer(ticket, time, deltav):
@@ -136,9 +154,8 @@ def submit_answer(ticket, time, deltav):
                 s.sendall(str(deltav[2]).encode())
 
 
-
 ticket = 'ticket{papa368346yankee2:GH7VByudpDcHDBMaEAt0SxE3iZ3v_QkahVqj9T0RhNPdTPvW_STlg28vsLW1Pj6Eqw}'
-print(ticket)
+# print(ticket)
 
 # https://www.sciencedirect.com/topics/engineering/perigee-passage
 
@@ -155,14 +172,23 @@ print(ticket)
 
 # a=42164+/-10km, e<0.001, i<1deg
 
-original_pos = [8449.401305, 9125.794363, -17.461357]
-original_vel = [-1.419072, 6.780149, 0.002865]
+input = read_input('inputs.json')
+original_pos = input['position']
+original_vel = input['velocity']
+
+print("           Pre-Maneuver Ephemeris             ")
+print("==============================================")
+print("Position: ", original_pos)
+print("Velocity vector: ", original_vel)
+print("==============================================")
+print()
 
 elements = OrbitalElements()
 elements.calculate(original_pos, original_vel)
 
 julian_epoch = datetime.datetime(1949, 12, 31, 0, 0, 0, tzinfo=datetime.timezone.utc)
-sat_epoch = datetime.datetime(2021, 6, 26, 19, 20, 0, tzinfo=datetime.timezone.utc)
+sat_epoch = datetime.datetime.strptime(input['epoch'], '%Y-%m-%d-%H:%M:%S.000-UTC')
+sat_epoch = sat_epoch.replace(tzinfo=datetime.timezone.utc)
 delta = sat_epoch - julian_epoch
 julian = delta.days + delta.seconds/86400
 
@@ -184,8 +210,12 @@ ret = sat.sgp4init(
 )
 
 line1, line2 = exporter.export_tle(sat)
+print("           Pre-Maneuver TLE                   ")
+print("==============================================")
 print(line1)
 print(line2)
+print("==============================================")
+print()
 
 # found by brute force, not exact calculation
 # propagated the satellite until the max distance from earth and the min velocity magnitude was found
@@ -206,46 +236,57 @@ error, r, v = sat.sgp4_array(np.array(jd_array), np.array(fr_array))
 apogee_distance = 0
 perigee_distance = 100000
 time_index = 0
-with open('out.txt', 'w') as fp:
-    for i in range(0, len(r)):
-        d = np.linalg.norm(r[i])
-        print("distance {} at time {}".format(d, time_array[i]), file=fp)
-        if d > apogee_distance:
-            apogee_distance = d
-            time_index = i
-        if d < perigee_distance:
-            perigee_distance = d
+time_index_perigee = 0
+for i in range(0, len(r)):
+    d = np.linalg.norm(r[i])
+    if d > apogee_distance:
+        apogee_distance = d
+        time_index = i
+    if d < perigee_distance:
+        perigee_distance = d
+        time_index_perigee = i
 
 # Velocity magnitude
 vv = np.linalg.norm(v[time_index])
 
-print("Apoapsis distance (km): ", apogee_distance)
-print("Min orbital velocity (km/s): ", vv)
-print("At time: ", time_array[time_index])
-print("Periapsis distance (km): ", perigee_distance)
+print("           Propagation Result                 ")
+print("==============================================")
+print("Apoapsis distance (km): {} at {}".format(apogee_distance, time_array[time_index]))
+print("Apoapsis velocity (km/s): ", vv)
+print("Periapsis distance (km): {} at {}".format(perigee_distance, time_array[time_index_perigee]))
+print("==============================================")
+print()
 
 # velocity unit vector
 v_hat = v[time_index] / vv
 # print(v_hat)
 
-target_eccentricity = 0.00099
+print("      Predicted Post-Maneuver Elements        ")
+print("==============================================")
+target_eccentricity = input['target_eccentricity'] - 0.00001
+print("Target Eccentricity, e: ", target_eccentricity)
 target_sma = (apogee_distance/(1+target_eccentricity))
-print("target semi-major axis (km)", target_sma)
+print("Target Semimajor axis, a (km): ", target_sma)
 target_velocity = vis_viva(apogee_distance, target_sma)
+print("==============================================")
+print()
 
-# alt_deltav = get_hohmann_dv2(42173.2, perigee_distance)
-# alt_deltav = alt_deltav/1000
-# print("alternate delta v", alt_deltav)
-# alt_dv_vector = np.multiply(v_hat, alt_deltav)
-
+print("           Maneuver Parameters                ")
+print("==============================================")
 final_dv = target_velocity/1000 - vv
-print("delta v", final_dv)
+print("Required delta v, dv (km/s): ", final_dv)
 final_dv_vector = final_dv*v_hat
-print("delta v vector", final_dv_vector)
-print("position vector", r[time_index])
+print("Delta v vector: ", final_dv_vector)
+print("Maneuver time: ", time_array[time_index])
+print("==============================================")
+print()
+
+print("           Post-Maneuver Ephemeris            ")
+print("==============================================")
+print("Position: ", r[time_index])
 new_velocity_vector = np.add(v[time_index], final_dv_vector)
-print("new velocity vector", new_velocity_vector)
-# print("new alternate velocity vector", np.add(v[time_index], alt_dv_vector))
+print("New velocity vector: ", new_velocity_vector)
+print("==============================================")
 print()
 # submit_answer(ticket, tt, final_dv_vector)
 
@@ -271,6 +312,31 @@ ret = newSat.sgp4init(
     newElements.mm,             # no_kozai: mean motion (radians/minute)
     newElements.omega           # nodeo: right ascension of ascending node (radians)
 )
+
+print("           Post-Maneuver TLE                  ")
+print("==============================================")
 line1, line2 = exporter.export_tle(newSat)
 print(line1)
 print(line2)
+print("==============================================")
+print()
+
+print("           Post-Maneuver Analysis             ")
+print("==============================================")
+if (newElements.semimajor_axis/1000 < float(input['target_semi_major_axis']+input['target_sma_error']) and
+    newElements.semimajor_axis/1000 > float(input['target_semi_major_axis']-input['target_sma_error'])):
+    print("Post-Maneuver semi-major axis within defined parameters")
+else:
+    print("Post-Maneuver semi-major axis outside of defined parameters!")
+
+if newElements.eccentricity < input['target_eccentricity']:
+    print("Post-Maneuver eccentricity within defined parameters")
+else:
+    print("Post-Maneuver eccentricity outside of defined parameters!")
+
+if newElements.inclination < input['target_inclination']:
+    print("Post-Maneuver inclination within defined parameters")
+else:
+    print("Post-Maneuver inclination outside of defined parameters!")
+print("==============================================")
+print()
